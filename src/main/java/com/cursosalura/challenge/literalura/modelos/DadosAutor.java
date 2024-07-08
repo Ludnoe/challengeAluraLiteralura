69b4fa3622e4eed7cf38cb25fb63a56b0cb62a63
Danilo Silva <ludnoe@hotmail.com>
2024-07-07 23:15:40 -0300

    implantação dos pacotes e classes e db

--- src/main/java/com/cursosalura/challenge/literalura/modelos/DadosAutor.java
+++ src/main/java/com/cursosalura/challenge/literalura/modelos/DadosAutor.java
@@ -1,11 +1,42 @@
 package com.cursosalura.challenge.literalura.modelos;
 
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import org.springframework.data.annotation.Id;
+
+import java.util.Objects;
+
+@Entity
 public class DadosAutor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String nome;
-    private int anoNascimento;
+    private Integer anoNascimento;
     private Integer anoFalecimento; // Pode ser nulo se o autor ainda estiver vivo
 
+    // Construtores
+    public DadosAutor() {}
+
+    public DadosAutor(String nome, Integer anoNascimento, Integer anoFalecimento) {
+        this.nome = nome;
+        this.anoNascimento = anoNascimento;
+        this.anoFalecimento = anoFalecimento;
+    }
+
     // Getters e setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getNome() {
         return nome;
@@ -15,11 +46,11 @@ public class DadosAutor {
         this.nome = nome;
     }
 
-    public int getAnoNascimento() {
+    public Integer getAnoNascimento() {
         return anoNascimento;
     }
 
-    public void setAnoNascimento(int anoNascimento) {
+    public void setAnoNascimento(Integer anoNascimento) {
         this.anoNascimento = anoNascimento;
     }
 
@@ -29,5 +60,28 @@ public class DadosAutor {
 
     public void setAnoFalecimento(Integer anoFalecimento) {
         this.anoFalecimento = anoFalecimento;
+    }
+
+    // toString, equals, and hashCode
+    @Override
+    public String toString() {
+        return "Autor{" +
+                "nome='" + nome + '\'' +
+                ", anoNascimento=" + anoNascimento +
+                ", anoFalecimento=" + anoFalecimento +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DadosAutor autor = (DadosAutor) o;
+        return Objects.equals(id, autor.id) && Objects.equals(nome, autor.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nome);
     }
 }
--- src/main/java/com/cursosalura/challenge/literalura/modelos/DadosLivros.java
+++ src/main/java/com/cursosalura/challenge/literalura/modelos/DadosLivros.java
@@ -1,4 +1,98 @@
 package com.cursosalura.challenge.literalura.modelos;
 
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import org.springframework.data.annotation.Id;
+
+import java.util.Objects;
+
+@Entity
 public class DadosLivros {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String titulo;
+    private String autor; // Apenas o nome do autor
+    private String idioma;
+    private int numeroDownloads;
+
+    // Construtores
+    public DadosLivros() {}
+
+    public DadosLivros(String titulo, String autor, String idioma, int numeroDownloads) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.idioma = idioma;
+        this.numeroDownloads = numeroDownloads;
+    }
+
+    // Getters e setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getIdioma() {
+        return idioma;
+    }
+
+    public void setIdioma(String idioma) {
+        this.idioma = idioma;
+    }
+
+    public int getNumeroDownloads() {
+        return numeroDownloads;
+    }
+
+    public void setNumeroDownloads(int numeroDownloads) {
+        this.numeroDownloads = numeroDownloads;
+    }
+
+    // toString, equals, and hashCode
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "titulo='" + titulo + '\'' +
+                ", autor='" + autor + '\'' +
+                ", idioma='" + idioma + '\'' +
+                ", numeroDownloads=" + numeroDownloads +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DadosLivros livro = (DadosLivros) o;
+        return Objects.equals(id, livro.id) && Objects.equals(titulo, livro.titulo);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, titulo);
+    }
 }
